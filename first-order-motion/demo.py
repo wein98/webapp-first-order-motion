e48f034cc7b9985b1d617208ce0b3435753ff0c4
wein98 <wein9298@hotmail.com>
2021-05-08 15:52:29 +0800

    ADDED: stage 2 first order motion model code

--- first-order-motion/demo.py
+++ first-order-motion/demo.py
@@ -1,4 +1,5 @@
 import stage1
+import stage2
 
 # pipeline for stage-1: input preprocessing
 # 1. get video and crop it
@@ -7,3 +8,9 @@ pathIn= '/'
 pathOut = 'video.mp4'
 videoFile = 'singing_2.mp4'
 stage1.cropVideo(videoFile, pathIn, pathOut)
+
+# stage-2: image animating
+# put cropped source image to first-order-motion/ and name it sourceImage.jpg
+imagePath = 'sourceImage.jpg'
+videoPath = pathOut
+stage2.generate_output(imagePath, videoPath)
--- first-order-motion/frames/src_haha/detector.py
+++
@@ -1,2 +0,0 @@
-def kek(bla):
-    print(bla)
